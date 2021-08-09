e745ac252fd8d3e46f0882031a9c6a16c350d707
Arden Kirkland <arden@ardenkirkland.com>
2021-08-09 08:13:16 -0700

    small edits but still not quite working

--- views/public/storage_table.php
+++ views/public/storage_table.php
@@ -1,34 +1,34 @@
-<div>
-    <table>
-        <tr>
-            <td>Storage Location</td>
-            <td>Identifier</td>
-            <td>Title</td>
-            <td>Description</td>
-        </tr>
-        <?php foreach($items as $item): ?>
-            <tr>
-            <td>
-                <?php if (metadata('item', array('Item Type Metadata', 'Storage Location')) !='') {
-                    echo metadata('item', array('Item Type Metadata', 'Storage Location'));
-                    } ?>
-            </td>
-            <td>
-                <?php if (metadata('item', array('Dublin Core', 'Identifier')) !='') {
-                    echo metadata('item', array('Dublin Core', 'Identifier'));
-                    } ?>
-            </td>
-            <td>
-                <?php if (metadata('item', array('Dublin Core', 'Title')) !='') {
-                    echo metadata('item', array('Dublin Core', 'Title'));
-                    } ?>
-            </td>
-            <td>
-                <?php if (metadata('item', array('Dublin Core', 'Description')) !='') {
-                    echo metadata('item', array('Dublin Core', 'Description'));
-                    } ?>
-            </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
+        <div>
+            <table>
+                <tr>
+                    <td>Storage Location</td>
+                    <td>Identifier</td>
+                    <td>Title</td>
+                    <td>Description</td>
+                </tr>
+                <?php foreach($items as $item): ?>
+                    <tr>
+                    <td>
+                        <?php if (metadata('item', array('Item Type Metadata', 'Storage Location')) !='') {
+                            echo metadata('item', array('Item Type Metadata', 'Storage Location'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Identifier')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Identifier'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Title')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Title'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Description')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Description'));
+                            } ?>
+                    </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
