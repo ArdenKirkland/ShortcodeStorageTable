60fb083d0dc629fa90b80d107288d8df88be51ae
Arden Kirkland <arden@ardenkirkland.com>
2021-08-11 20:32:20 +0000

    interim changes, still didn't work

--- views/public/storage_table.php
+++ views/public/storage_table.php
@@ -1,12 +1,36 @@
-<div>
-    <table>
-        <?php foreach($items as $item): ?>
-            <tr>
-            <td><?php echo item('Item Type Metadata', 'Storage Location'); ?></td>
-            <td><?php echo item('Dublin Core', 'Identifier'); ?></td>
-            <td><?php echo item('Dublin Core', 'Title'); ?></td>
-            <td><?php echo item('Dublin Core', 'Description'); ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
+        <div>
+            <table>
+            <tbody>
+                <tr>
+                    <td>Storage Location</td>
+                    <td>Identifier</td>
+                    <td>Title</td>
+                    <td>Description</td>
+                </tr>
+                <?php foreach (loop ('items') as $item): ?>
+                    <tr>
+                    <td>
+                        <?php if (metadata('item', array('Item Type Metadata', 'Storage Location')) !='') {
+                            echo metadata('item', array('Item Type Metadata', 'Storage Location'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Identifier')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Identifier'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Title')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Title'));
+                            } ?>
+                    </td>
+                    <td>
+                        <?php if (metadata('item', array('Dublin Core', 'Description')) !='') {
+                            echo metadata('item', array('Dublin Core', 'Description'));
+                            } ?>
+                    </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+            </table>
+        </div>
